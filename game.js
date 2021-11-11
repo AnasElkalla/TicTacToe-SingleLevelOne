dd61d03a8ea44c7ca78de465c08c5d6d1bf14063
AnasElkalla <79075490+AnasElkalla@users.noreply.github.com>
2021-11-11 22:27:40 +0200

    Update game.js

--- game.js
+++ game.js
@@ -55,7 +55,7 @@ menu.addEventListener("click", function () {
  
 });
 exit.addEventListener("click", function (e) {
-  window.close("", "_parent", "_");
+  window.location="https://anaselkalla.github.io/TicTacToeHome/";
 });
 newGame.addEventListener("click", function () {
   makeSound();
